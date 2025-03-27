39b2b985b7c380616fcca37611d27df893b995a8
Eugene <evg.v38@gmail.com>
2025-03-27 23:07:36 +1000

    * filament some settings

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements FilamentUser
+//class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -45,8 +46,8 @@ class User extends Authenticatable implements FilamentUser
         'password' => 'hashed',
     ];
 
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
-    }
+//    public function canAccessPanel(Panel $panel): bool
+//    {
+//        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+//    }
 }
